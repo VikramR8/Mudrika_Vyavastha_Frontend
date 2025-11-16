1b5ce683030863e57a7e90fe5d4984b00caae042
VikramR8 <vikramadityareshmi@gmail.com>
2025-11-16 20:52:56 +0530

    request and response interceptors implemented

---
+++ src/util/axiosConfig.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const axiosConfig = axios.create({
+    baseURL: "https://mudrika-vyavastha-backend.onrender.com/api/v1.0",
+    headers : {
+        "Content-Type": "application/json",
+        Accept:"application/json"
+    }
+});
+
+const excludeEndPoints = ["/login", "/register", "/status", "/activate", "/health"];
+
+
+//Request Interceptors
+axiosConfig.interceptors.request.use((config)=>{
+    const shouldSkipToken = excludeEndPoints.some((endpoint)=>{
+        config.url?.includes(endpoint)
+    })
+
+    if(!shouldSkipToken) {
+        const accessToken = localStorage.getItem("token");
+        if(accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+    }
+
+    return config
+}, (error)=>{
+    return Promise.reject(error)
+})
+
+//Response Interceptors
+axiosConfig.interceptors.response.use((response)=>{
+    return response;
+}, (error)=>{
+    if (error.response) {
+        if(error.response.status === 401){
+            window.location.href = "/login"
+        }
+        else if(error.response.status === 500){
+            console.log("Server error. Please try again later!")
+        }
+    } else if(error.code === "ECONNABORTED") {
+        console.log("Request timeout. Please")
+    }
+    return Promise.reject(error)
+})
